83ad22996f534e7f9d60554d307dc9ded253e292
chubi-x <michaeladejoh01@gmail.com>
2021-02-25 13:48:12 +0400

    created and configured service worker

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+var cacheName = 'petstore-v1';
+var cacheFiles = [
+    'index.html',
+    'products.js',
+    'petstore.webmanifest',
+    'images/1.jpg',
+    'images/2.jpg',
+    'images/3.jpg',
+    'images/4.jpg',
+    'images/5.jpg',
+    'images/6.jpg',
+    'images/icon-store-512.jpg'
+];
+
+self.addEventListener('install',(e)=>{
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+      caches.open(cacheName).then((cache)=>{
+          console.log('[Service Worker] Caching all files...');
+          return cache.addAll(cacheFiles);
+      })
+    
+  )
+})
